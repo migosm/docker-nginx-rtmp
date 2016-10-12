3e5f8d0a77064f1db8bdbb57a7f867839d0fb4ed
Mikhail Osipov <mikhail.osipov@lazada.com>
2016-10-12 16:29:06 +0700

    update run.sh; add example configs

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 case $1 in
   start)
     sudo docker run -td \
-      -p 80:80 -p 1935:1935 \
+      -p 80:8080 \
+      -p 1935:1935 \
       --name=nr1 \
       -v /opt/data:/data \
+      -v /opt/configs/nginx:/etc/nginx
       migosm/docker-nginx-rtmp \
       bash
   ;;
